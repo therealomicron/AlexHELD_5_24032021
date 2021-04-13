52023c6c1d63b918208d77fce6dc694dba20767a
Alex Held <heldalex@outlook.com>
2021-04-13 09:43:14 +0200

    URLSearchParams functional

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,5 +1,5 @@
 let url = location;
 let searchParams = new URLSearchParams(url.search);
- pID = String(searchParams).slice(-1);
-//alert(pID);
-const productAPI = http://localhost:3000/api/teddies; 
+let pID = searchParams.get("pid");
+alert(pID);
+const productAPI = "http://localhost:3000/api/teddies";
